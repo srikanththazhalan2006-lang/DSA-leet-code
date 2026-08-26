5b4c49a9e381607a6f9af87babe48e015dca6e29
srikanth2006 <srikanththazhalan2006@gmail.com>
2026-08-26 18:39:02 +0530

    Time: 923 ms (8.11%) | Memory: 11.6 MB (89.86%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,40 +1,19 @@
 class Solution {
+
 public:
-  int solve(vector<int>& nums, int target, int index, int sum) {
-
-        // We have used all the numbers
+    int solve(vector<int>& nums, int target, int index, int sum) {
         if (index == nums.size()) {
-
-            // If our sum became target, this is 1 valid way
-            if (sum == target)
-                return 1;
-
-            // Otherwise, this way is useless
-            return 0;
+            return sum == target ? 1 : 0;
         }
 
-        // Put + before nums[index]
-        int add = solve(
-            nums,
-            target,
-            index + 1,
-            sum + nums[index]
-        );
+        int add = solve(nums, target, index + 1, sum + nums[index]);
+        int sub = solve(nums, target, index + 1, sum - nums[index]);
 
-        // Put - before nums[index]
-        int subtract = solve(
-            nums,
-            target,
-            index + 1,
-            sum - nums[index]
-        );
-
-        // Count both possibilities
-        return add + subtract;
+        return add + sub;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         return solve(nums, target, 0, 0);
-        
     }
 };
+        
